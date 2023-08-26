e6c838bf0b5ad9918d9ee84760026b6f378a4dae
Albert Albala <albert@mediatribe.net>
2023-08-26 10:48:13 -0400

    Please squash this commit in the pull request

--- drupal/custom-modules/my_custom_module/src/App.php
+++ drupal/custom-modules/my_custom_module/src/App.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\my_custom_module;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\my_custom_module\traits\Environment;
 use Drupal\my_custom_module\traits\Singleton;
 
@@ -10,6 +11,7 @@ use Drupal\my_custom_module\traits\Singleton;
  */
 class App {
 
+  use StringTranslationTrait;
   use Singleton;
   use Environment;
 
@@ -18,6 +20,14 @@ class App {
    */
   public function hookCron() {
     // Just an example of where you'd implement testable hooks.
+  }
+
+  public function hookPermission() : array {
+    return [
+      'administer franchises' => [
+        'title' => $this->t('Administer franchises'),
+      ],
+    ];
   }
 
 }
